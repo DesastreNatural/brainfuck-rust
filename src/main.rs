9b6f1c782a26a8fe79a71ba7b9889feb2687e18f
Michele <link89mb@gmail.com>
2024-01-27 13:02:47 +0100

    semi-stable implementation, now working for cleaning some things

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use clap::Parser;
 use std::path::Path;
 use std::fs;
-
 pub mod interpreter {
+
+    use getch::Getch;
+    use std::process::exit;
+    use std::io::{self, Write};
     pub struct Interpreter {
         code_source: Vec<char>,
         code_ptr: i32,
         mem_ptr: i32,
         mem_ptr_shift: i32,
-        memory: Vec<i32>,
+        memory: Vec<u8>,
+        running: bool,
     }
 
     impl Interpreter {
@@ -19,15 +23,26 @@ pub mod interpreter {
                 code_ptr: 0,
                 mem_ptr: 0,
                 mem_ptr_shift: 0,
-                memory: vec![0] 
+                memory: vec![0],
+                running: true
             }
         }
-        pub fn print_memory(&self){ println!("{:?}",&self.memory); }
-        pub fn inc_mem_value(&mut self){ 
-            self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] += 1;
+        pub fn print_memory(&self){
+            println!("{:?}",&self.memory);
+        }
+        pub fn inc_mem_value(&mut self){
+            if self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] == 255 {
+                self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] = 0;
+            } else {
+                self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] += 1;
+            }
         }
         pub fn dec_mem_value(&mut self){
-            self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] -= 1;
+            if self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] == 0 {
+                self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] = 255;
+            } else {
+                self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] -= 1;
+            }
         }
         pub fn inc_mem_ptr(&mut self){
             self.mem_ptr += 1;
@@ -42,8 +57,72 @@ pub mod interpreter {
                 self.memory.insert(0, 0)
             }
         }
-        pub fn output_ptr(){ todo!(); }
-        pub fn input_ptr(){ todo!(); }
+        pub fn output_ptr(&self){
+            print!("{}", self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] as char);
+            io::stdout().flush().unwrap();
+        }
+        pub fn input_ptr(&mut self){ 
+            let f = Getch::new();
+            self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] = f.getch().unwrap();
+        }
+        pub fn jump_if_zero(&mut self){
+            if self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] == 0 {
+                let mut parity_point = -1;
+                while parity_point != 0 {
+                    self.code_ptr += 1;
+                    if self.code_ptr == self.code_source.len() as i32 {
+                        eprintln!("unmatched [");
+                        exit(1);
+                    }
+                    if self.code_source[self.code_ptr as usize] == '[' {
+                        parity_point -= 1
+                    } else if self.code_source[self.code_ptr as usize] == ']' {
+                        parity_point += 1
+                    }
+                }
+            }
+        }
+        pub fn jump_back_if_not_zero(&mut self){
+            if self.memory[(self.mem_ptr + self.mem_ptr_shift) as usize] != 0 {
+                let mut parity_point = -1;
+                while parity_point != 0 {
+                    self.code_ptr -= 1;
+                    if self.code_ptr < 0 {
+                        eprintln!("unmatched ]");
+                        exit(1);
+                    }
+                    if self.code_source[self.code_ptr as usize] == ']' {
+                        parity_point -= 1
+                    } else if self.code_source[self.code_ptr as usize] == '[' {
+                        parity_point += 1
+                    }
+                }
+            }
+        }
+        pub fn eval_ptr(&mut self){
+            if self.code_ptr >= self.code_source.len() as i32 {
+                self.running = false;
+            } else {
+                match self.code_source[self.code_ptr as usize] {
+                    '<' => self.dec_mem_ptr(),
+                    '>' => self.inc_mem_ptr(),
+                    '+' => self.inc_mem_value(),
+                    '-' => self.dec_mem_value(),
+                    '[' => self.jump_if_zero(),
+                    ']' => self.jump_back_if_not_zero(),
+                    '.' => self.output_ptr(),
+                    ',' => self.input_ptr(),
+                    _ => todo!()
+                }
+            }
+        }
+        pub fn interpret(&mut self){
+            while(self.running){
+                self.eval_ptr();
+                self.code_ptr += 1;
+            }
+            print!("\n");
+        }
     }
 }
 
@@ -63,19 +142,7 @@ fn main() {
         let allowed_tokens: Vec<char> = ['<','>','+','-','[',']','.',','].to_vec();
         let data: String = raw_data.chars().filter(|&c| allowed_tokens.contains(&c)).collect();
         let mut bf: Interpreter = Interpreter::created(data.chars().collect());
-        println!("{}", data);
-        bf.print_memory(); 
-        bf.inc_mem_ptr();
-        bf.inc_mem_value();
-        bf.inc_mem_ptr();
-        bf.inc_mem_value();
-        bf.dec_mem_ptr();
-        bf.dec_mem_ptr();
-        bf.dec_mem_ptr();
-        bf.dec_mem_ptr();
-        bf.inc_mem_ptr();
-        bf.print_memory();
-        
+        bf.interpret();
     } else {
         eprintln!("Not a valid file.")
     }
